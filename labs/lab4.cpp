541ad1bea35dd054477e51b4d323eaa09c6237cc
Momen959 <momen.saif22@gmail.com>
2025-10-30 11:52:31 +0300

    next addition

--- labs/lab4.cpp
+++ labs/lab4.cpp
@@ -3,11 +3,20 @@
 
 class stack {
     // int arr[size];
+    int top;
     stack(int size){
-        
+        top = -1;
+    }
+    int push(int value){
+
+    }
+    int pop(){
+
     }
 }
 
 int main{
-
+    int size;
+    cin >> size;
+    stack(size)
 }
